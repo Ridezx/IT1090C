7bbaff295e8dd393b61fcbb24721bda6bd42a159
Ridez <letitridez.magic@gmail.com>
2025-09-15 22:52:03 -0400

    Part2Complete

---
+++ src/Part2Task4.java
@@ -0,0 +1,10 @@
+public class Part2Task4 {
+    public static void main(String[] args) {
+        int num = 2;
+
+        if (num % 2 == 0)
+        {System.out.println("The number " + num + " is even.");}
+        else
+        {System.out.println("The number " + num + " is odd.");}
+    }
+}
